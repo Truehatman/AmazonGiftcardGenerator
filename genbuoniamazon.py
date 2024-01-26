ede921154cfce858181edc87006d6582730f61f9
hatmanexchange <156363293+hatmanexchange@users.noreply.github.com>
2024-01-26 13:57:25 +0100

    Add files via upload

---
+++ genbuoniamazon.py
@@ -0,0 +1,35 @@
+import random
+import string
+import time
+import sys
+
+def generate_random_string(length):
+    characters = string.ascii_letters + string.digits
+    return ''.join(random.choice(characters) for _ in range(length))
+
+num_strings = 10
+string_length = 16
+
+while True:
+    print("\nMenu:")
+    print("1. Generate Amazon Giftcard")
+    print("2. End Software")
+
+    user_choice = input("Chose the number: ")
+
+    if user_choice == "1":
+        print('Generating giftcard...')
+        time.sleep(5)
+
+        print("\nAmazon Giftcard Generated:\n")
+        for i in range(num_strings):
+            new_string = generate_random_string(string_length)
+            print(new_string)
+            time.sleep(3)
+        
+    elif user_choice == "2":
+        print("Generator terminated.")
+        break
+
+    else:
+        print("Command don't exist")
